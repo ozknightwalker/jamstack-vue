22dcafc8a2e6ee4573b3336cf6e5a0bf5cc5f496
Jc Din <ozknightwalker62@gmail.com>
2019-11-19 11:18:43 +0800

    setup pwa config

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,29 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Gridsome VueJS',
+  icon: {
+    favicon: './src/favicon.png',
+    touchicon: './src/favicon.png'
+  }
+  plugins: [
+    {
+      use: 'gridsome-plugin-pwa',
+      options: {
+        title: 'Gridsome VueJS',
+        startUrl: '/',
+        display: 'standalone',
+        statusBarStyle: 'default',
+        manifestPath: 'manifest.json',
+        serviceWorkerPath: 'service-worker.js',
+        cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
+        shortName: 'Gridsome VueJS',
+        themeColor: '#666600',
+        backgroundColor: '#ffffff',
+        icon: './src/favicon.png',
+        msTileImage: './src/favicon.png',
+        msTileColor: '#666600'
+      }
+    }
+  ]
 }

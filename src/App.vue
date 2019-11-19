7925cd3ee08f2c26db2c847dad18ed28c3a66b45
Jc Din <ozknightwalker62@gmail.com>
2019-11-19 11:19:17 +0800

    remove readme files and organized vue components also add 404 handler

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="app" id="app">
+        <Header />
+        <div class="container">
+            <router-view />
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+export default {
+
+    name: 'App',
+
+    data() {
+        return {};
+    },
+
+    components: {
+        Header: () =>
+            import ("~/components/Header.vue"),
+        Footer: () =>
+            import ("~/components/Footer.vue"),
+    },
+};
+</script>
+
+<style lang="css" scoped>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+}
+
+.app {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,32 +1,37 @@
 <template>
-  <Layout>
+    <div class="index">
+        <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+        <h1>Hello, world!</h1>
 
-    <h1>Hello, world!</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+        </p>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+        <p class="home-links">
+            <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+            <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+        </p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+    </div>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+
+    name: 'Index',
+
+    metaInfo: {
+        title: 'Hello, world!'
+    },
+
+    data() {
+        return {};
+    },
+};
 </script>
 
-<style>
+<style lang="css" scoped>
 .home-links a {
   margin-right: 1rem;
 }
